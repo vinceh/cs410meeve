88a9ce4b730704841f86abbc47bc25b5c3363ad2
unknown <Vince@.(none)>
2010-11-13 03:16:47 -0800

    Started on the sign up form

--- meeve/public/javascripts/application.js
+++ meeve/public/javascripts/application.js
@@ -1,5 +1,23 @@
 // Place your application-specific JavaScript functions and classes here
 // This file is automatically included by javascript_include_tag :defaults
+$(window).load(function () {
+	$(".signup_form input").bind({
+		  focusin: function(){
+          $(this).css("border", "1px solid black");
+      },
+      focusout: function(){
+          $(this).css("border", "1px solid #9a9a9a");
+      }
+	});
+    $("#signup_form input").toggle(
+        function () {
+            $(this).css("height","15em");
+        },
+        function () {
+            $(this).css("height","");
+        }
+    );
+});
 
 (function($){
     $.fn.hintedInput = function(hint) {
