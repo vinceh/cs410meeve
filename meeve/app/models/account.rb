922ee7ec885dafd573ca64caade70b30504a4f94
unknown <Vince@.(none)>
2010-12-02 18:30:36 -0800

    Added account inactivity and some password and email validation

--- meeve/app/models/account.rb
+++ meeve/app/models/account.rb
@@ -7,4 +7,20 @@ class Account < ActiveRecord::Base
 	  validates_confirmation_of :password
 	  
   	  validates_presence_of :first_name, :last_name, :email, :password
+  	  
+  	  validate :password_strength, :email_valid
+  	  
+  	  def password_strength
+  	  	
+  	  	if password.length < 6
+  	  		errors.add(:password, " not 6 characters")
+  	  	end
+  	  end
+  	  
+  	  def email_valid
+  	  	
+  	  	if !(email =~ /^([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})$/i)
+  	  		errors.add(:email, " is not a valid email")
+  	  	end
+  	  end
 end
